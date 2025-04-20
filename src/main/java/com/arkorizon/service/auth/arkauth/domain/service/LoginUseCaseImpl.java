26805ac14546d0a1c7d3a8a4302a773891b4ff95
Damian Lourenco <noah.damian.tech@gmail.com>
2025-04-20 12:20:54 -0300

    refactor(core): remove @Service from use case impl to align with hexagonal architecture

--- src/main/java/com/arkorizon/service/auth/arkauth/domain/service/LoginUseCaseImpl.java
+++ src/main/java/com/arkorizon/service/auth/arkauth/domain/service/LoginUseCaseImpl.java
@@ -6,20 +6,21 @@ import com.arkorizon.service.auth.arkauth.domain.model.result.AuthTokenResult;
 import com.arkorizon.service.auth.arkauth.domain.port.in.LoginUseCase;
 import com.arkorizon.service.auth.arkauth.domain.port.out.TokenServicePort;
 import com.arkorizon.service.auth.arkauth.domain.port.out.UserQueryPort;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
 public class LoginUseCaseImpl implements LoginUseCase {
 
-    private final UserQueryPort userQueryPort;
-    private final TokenServicePort tokenServicePort;
+  private final UserQueryPort userQueryPort;
+  private final TokenServicePort tokenServicePort;
 
-    @Override
-    public AuthTokenResult login(final LoginCommand command) {
-        final User user = userQueryPort.findByEmailAndGroup(command.email(), command.groupId(), command.subGroupId());
-        tokenServicePort.validatePassword(command.password(), user.password());
-        return tokenServicePort.generateToken(user);
-    }
+  public LoginUseCaseImpl(UserQueryPort userQueryPort, TokenServicePort tokenServicePort) {
+    this.userQueryPort = userQueryPort;
+    this.tokenServicePort = tokenServicePort;
+  }
+
+  @Override
+  public AuthTokenResult login(final LoginCommand command) {
+    final User user = userQueryPort.findByEmailAndGroup(command.email(), command.groupId(), command.subGroupId());
+    tokenServicePort.validatePassword(command.password(), user.password());
+    return tokenServicePort.generateToken(user);
+  }
 }
